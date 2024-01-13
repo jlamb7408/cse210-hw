721c9765d064459c49e14b762898d027c22aa810
jlamb7408 <jlamb7408@gmail.com>
2024-01-12 22:50:04 -0700

    Wrote the code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+
+        Console.WriteLine("Enter your precentage in the class: ");
+        string str_percentage = Console.ReadLine();
+        int int_percentage = int.Parse(str_percentage);
+
+        if (int_percentage == 90 || int_percentage > 90)
+        {
+            letter = "A";
+        }
+        else if (int_percentage == 80 || int_percentage > 80)
+        {
+            letter = "B";
+        }
+        else if (int_percentage == 70 || int_percentage > 70)
+        {
+            letter = "C";
+        }
+        else if (int_percentage == 60 || int_percentage > 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got an {letter} in the class");
+        if (int_percentage == 70 || int_percentage > 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class");
+        }
+
     }
 }
