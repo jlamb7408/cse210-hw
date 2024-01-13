a3d84d304b3c1164b64d1be3570f47b1b9dd74b9
jlamb7408 <jlamb7408@gmail.com>
2024-01-12 22:09:50 -0700

    wrote all the code so far

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is  your first name? ");
+        string first_name = Console.ReadLine();
+        
+        Console.WriteLine("What is  your last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
     }
 }
+
+/*
+What is your first name? Scott
+What is your last name? Burton
+
+Your name is Burton, Scott Burton.
+*/
