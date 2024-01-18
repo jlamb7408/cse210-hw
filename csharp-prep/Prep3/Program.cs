255e9e50ad7c96c7fd4daa68c74e8ecca357bce6
jlamb7408 <jlamb7408@gmail.com>
2024-01-17 17:41:33 -0700

    Wrote all the code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,35 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess_counter = 1;
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("Guess a number: ");
+        string str_user_guess = Console.ReadLine();
+        int user_guess = int.Parse(str_user_guess);
+
+        while (user_guess !=  magic_number)
+        {
+            if (magic_number > user_guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            if (magic_number < user_guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.WriteLine("Guess a number: ");
+            str_user_guess = Console.ReadLine();
+            user_guess = int.Parse(str_user_guess);
+            guess_counter ++;
+        }
+
+        Console.WriteLine("That's the number!");
+        Console.WriteLine($"It took you {guess_counter} guesses.");
+
     }
 }
