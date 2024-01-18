659637b8211f75d3e610c9bb6e9a3e5b2ff94de1
jlamb7408 <jlamb7408@gmail.com>
2024-01-17 22:48:10 -0700

    Code writtten

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int new_number = -1;
+        List<int> numbers = new List<int>();
+
+        while (new_number != 0)
+        {
+            Console.WriteLine("Enter a number to add to the list");
+            string str_new_number = Console.ReadLine();
+            new_number = int.Parse(str_new_number);
+            if (new_number != 0)
+            {
+                numbers.Add(new_number);
+            }
+            
+
+        }
+
+        int sum = 0;
+        int largest = 0;
+        foreach (int number in numbers)
+        {
+        sum += number;
+        if (number > largest)
+        {
+            largest = number;
+        }
+        }
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The sum is {sum}.");
+        Console.WriteLine($"The average is {average}.");
+        Console.WriteLine($"The largest  number is {largest}.");
+
+
+        
     }
 }
