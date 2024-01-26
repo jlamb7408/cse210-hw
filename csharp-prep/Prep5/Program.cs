45c0e6f1f3fa594839249880b695d69a8d1abe1e
jlamb7408 <jlamb7408@gmail.com>
2024-01-25 20:22:31 -0700

    code written

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,21 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.WriteLine("Enter your user name: ");
+        string user_name = Console.ReadLine();
+        return user_name;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string user_name = GetUserName();
+        
     }
 }
