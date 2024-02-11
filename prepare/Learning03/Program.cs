01e1891e704835ee27d60bdeb9a49fb2db3050de
jlamb7408 <jlamb7408@gmail.com>
2024-02-10 23:43:01 -0700

    did it

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction F1 = new Fraction();
+        Fraction F5 = new Fraction(5);
+        Fraction F3o4 = new Fraction (3,4);
+
+        double one_deci = F1.GetFractionValue();
+        string one_str = F1.GetFractionString();
+        Console.WriteLine(one_deci);
+        Console.WriteLine(one_str);
+
+        double five_deci = F5.GetFractionValue();
+        string five_str = F5.GetFractionString();
+        Console.WriteLine(five_deci);
+        Console.WriteLine(five_str);
+
+        double three_fourths_deci = F3o4.GetFractionValue();
+        string three_fourths_str = F3o4.GetFractionString();
+        Console.WriteLine(three_fourths_deci);
+        Console.WriteLine(three_fourths_str);
+
+
+
     }
 }
