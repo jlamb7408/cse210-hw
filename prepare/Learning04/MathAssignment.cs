037b6c47db0f656658e3777ea344594093026049
jlamb7408 <jlamb7408@gmail.com>
2024-03-02 14:30:57 -0700

    wrote classes

--- prepare/Learning04/MathAssignment.cs
+++ prepare/Learning04/MathAssignment.cs
@@ -2,10 +2,12 @@ public class MathAssignment : Assignment
 {
     private string _textbookSection;
     private string _problems;
+    private string _name = "name";
     public MathAssignment(string name, string topic, string textbookSection, string problems) : base(name,topic)
     {
         _textbookSection = textbookSection;
         _problems = problems;
+    
     }
     public string getMathHomeWork()
         {
