528a99fe2463c68b66bc656f5e01f4d3b32ebe68
jlamb7408 <jlamb7408@gmail.com>
2024-03-06 12:48:59 -0700

    finsihed assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        Square box = new Square("blue", 3);
+        Rectangle rect = new Rectangle("red", 3, 2);
+        Circle round = new Circle("yellow", 1);
+
+        List<Shape> shapes = new List<Shape>{box, rect, round};
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.getColor());
+            Console.WriteLine(s.getArea());
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,23 @@
+using System.Drawing;
+
+class Shape
+{
+    private string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public string getColor()
+    {
+        return _color;
+    }
+    public void _setColor(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double getArea()
+    {
+        return -1;
+    }
+}
